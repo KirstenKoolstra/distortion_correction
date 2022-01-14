b62858fc4a833d009c4b90e3ab59783b386feb5e
Kirsten Koolstra <64582629+KirstenKoolstra@users.noreply.github.com>
2022-01-14 17:43:13 +0100

    Update prepare_SB.m
    Changed m into n, needed for non-square computations

--- utils/prepare_SB.m
+++ utils/prepare_SB.m
@@ -3,7 +3,7 @@ function [ ER, RcEc, y1t, y2t ] = prepare_SB(E,R,y1,y2)
 global m n 
 
 % Prepare matrices and rhs
-ER  = zeros(size(E,1)*m,size(E,2));
+ER  = zeros(size(E,1)*n,size(E,2));
 y1t = zeros(m*n,1);
 y2t = zeros(m*n,1);
 for ind = 1:n
